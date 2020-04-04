0f58b1b7d08d1ce75347b68af9ff7ba554e9a939
Geon George <geongeorge98@gmail.com>
2020-04-04 10:32:14 +0530

    Updates

---
+++ precache-manifest.bcb1be8178c2c94d94b9e3223a095bd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ac15a282e55f5266ec1ab00a8ee310",
+    "url": "/Minimal-HN-React/index.html"
+  },
+  {
+    "revision": "baadf90dc416d73bf5e4",
+    "url": "/Minimal-HN-React/static/css/main.77f8186c.chunk.css"
+  },
+  {
+    "revision": "0babfe144a637717efeb",
+    "url": "/Minimal-HN-React/static/js/2.f2bad496.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/Minimal-HN-React/static/js/2.f2bad496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baadf90dc416d73bf5e4",
+    "url": "/Minimal-HN-React/static/js/main.35d04f3f.chunk.js"
+  },
+  {
+    "revision": "30d3d6d8e2b656623aca",
+    "url": "/Minimal-HN-React/static/js/runtime-main.1a634311.js"
+  }
+]);
